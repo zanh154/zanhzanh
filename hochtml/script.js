e6bfc068f05abaecd07656ded87bc772be6c65b1
zanh154 <nzero1542005@gmail.com>
2025-02-14 08:27:50 +0700

    Add files via upload

---
+++ hochtml/script.js
@@ -0,0 +1,10 @@
+let a=4;
+let b=4;
+let c=a+b;
+console.log(c);
+
+/*alert("Hellooo")*/
+confirm("Are you sure to continue?")
+/*hàm confirm nhấn ok nó mới tiếp tục những câu dưới còn bấm cancel là nó hủy luôn*/
+let ques = prompt("How are you?")
+console.log(ques);
